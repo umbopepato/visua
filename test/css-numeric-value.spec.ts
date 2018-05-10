b76b5937461d4588b0de31b812e587701c27fc16
Umberto Pepato <umbo.pepato@gmail.com>
2018-05-10 22:45:27 +0200

    Add CSSNumericValue#div() test

--- test/css-numeric-value.spec.ts
+++ test/css-numeric-value.spec.ts
@@ -27,15 +27,31 @@ describe('CSSNumericValue', () => {
             expect(() => CSS.px(12).mul(CSS.px(12)).to('px')).to.throw();
         });
 
-        it('should create a CSSUnitValue from a product between unit and number', function () {
+        it('should create a CSSUnitValue from a product between unit and number', () => {
             expect(CSS.px(12).mul(2)).to.be.deep.equal(CSS.px(24));
+        });
+
+    });
+
+    describe('#div', () => {
+
+        it('should create a CSSUnitValue from the division between unit and number', () => {
+            expect(CSS.mm(30).div(2).to('mm')).to.be.deep.equal(CSS.mm(15));
+        });
+
+    });
+
+    describe('#min', () => {
+
+        it('should create a CSSUnitValue from the minimum value provided', () => {
+            expect(CSS.mm(30).min(CSS.mm(15), CSS.mm(2))).to.be.deep.equal(CSS.mm(2));
         });
 
     });
 
     describe('#to()', () => {
 
-        it('should convert CSSUnitValue\'s with compatible units', function () {
+        it('should convert CSSUnitValue\'s with compatible units', () => {
             expect(CSS.px(10).to('cm').value).to.be.approximately(0.264583333, 0.000001);
         });
 
