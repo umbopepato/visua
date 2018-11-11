2f2241e8e2565247a3805fdb4868b440721876e6
Umberto Pepato <umbo.pepato@gmail.com>
2018-11-11 12:55:05 +0100

    Add colors and additional npm package.json fields to plugin command

--- src/cli/commands/plugin.ts
+++ src/cli/commands/plugin.ts
@@ -4,6 +4,7 @@ import {exec} from 'child_process';
 import * as fs from 'fs';
 import * as rimraf from 'rimraf';
 import * as path from 'path';
+import chalk from 'chalk';
 
 const asyncExec = util.promisify(exec);
 
@@ -13,25 +14,39 @@ const rlp = readline.createInterface({
 });
 
 export const plugin = async () => {
-    console.log('Creating a new empty visua plugin.');
-    console.log('See https://visua.io/guide/writing-plugins#naming for guidance on visua plugins naming conventions.\n');
+    console.log('Creating a new empty visua plugin.\n');
 
-    let slug = await rlp.questionAsync('Enter the plugin slug: visua-');
+    let slug = await rlp.questionAsync(b('Choose a slug for your plugin: ') + 'visua-');
     while (slug == null || slug.length === 0) {
-        slug = await rlp.questionAsync('Please provide a non empty slug: visua-');
+        slug = await rlp.questionAsync(b('Please provide a non empty slug: ') + 'visua-');
     }
-    let displayName = await rlp.questionAsync('Enter display name: ');
+    const pluginName = `visua-${slug}`;
+    let displayName = await rlp.questionAsync(b('Choose a display name for your plugin: '));
     while (!displayName || !displayName.length) {
-        displayName = await rlp.questionAsync('Please provide a non empty display name: ');
+        displayName = await rlp.questionAsync(b('Please provide a non empty display name: '));
     }
-    const description = await rlp.questionAsync('Enter a short description: ');
-    const pluginDir = path.join(process.cwd(), `visua-${slug}`);
+    const description = await rlp.questionAsync(b('Enter a short description: '));
+    const author = await rlp.questionAsync(b('Author: '));
+    const repository = await rlp.questionAsync(b('Git repository: '));
+    const pluginDir = path.join(process.cwd(), pluginName);
     console.log('Creating plugin directory');
     fs.mkdirSync(pluginDir);
-    console.log('Cloining starter template');
+    console.log('Cloning starter template');
     await asyncExec(`git clone https://github.com/umbopepato/visua-plugin-template ${pluginDir}`);
-    console.log('Removing versioning');
-    rimraf(path.join(pluginDir, '.git'), () =>
-    console.log(`Project succesfully created in folder visua-${slug}`));
-    process.exit();
+    console.log('Writing package.json');
+    const packagePath = path.join(pluginDir, 'package.json');
+    const pkg = require(packagePath);
+    pkg.name = pluginName;
+    pkg.description = description;
+    pkg.author = author;
+    pkg.repository = repository;
+    fs.writeFileSync(packagePath, JSON.stringify(pkg, null, 4));
+    console.log('Removing versioning\n');
+    rimraf(path.join(pluginDir, '.git'), () => {
+        console.log(`Project succesfully created in folder ${pluginName}!`);
+        console.log('See https://visua.io/guide/writing-plugins to learn more about visua plugins development');
+        process.exit();
+    });
 };
+
+const b = (str: string) => chalk.hex('#11adff')(str);
